72d18712381fd67e811cd5ed2289fe12d5cb8d40
Muhammad Shafiq ur Rehman <shafikrehman@gmail.com>
2024-10-05 00:22:35 +0500

    javascript done

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const myBtn = document.getElementById("myBtn");
+const myPopover = document.getElementById("myPopover");
+const closeBtn = document.getElementById("closeBtn");
+
+myBtn.addEventListener("click", function(){
+    myPopover.classList.add("active");
+});
+
+closeBtn.addEventListener("click", function(){
+    myPopover.classList.remove("active");
+});
